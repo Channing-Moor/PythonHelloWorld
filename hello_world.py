62bcebbecc05fff3bb96bb80ec1d88295f085dc7
Channing-Moor <crmoor01@bvsd.org>
2024-09-24 21:48:40 +0000

    Updated hello_world.py by asking user for name.

--- hello_world.py
+++ hello_world.py
@@ -1,8 +1,11 @@
-
-
+"""
+This is my first program. It output s "Hello World!" to the screen. 
+Channing Moor -- September 20 2024
+"""
 
 def main():
-  pass
+  name: str = input("Whats your name?: ") # type-hints: optional
+  print("Hello " + name + "!")   
 
 
 
